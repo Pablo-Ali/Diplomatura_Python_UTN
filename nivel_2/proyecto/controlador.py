7986cb8d123d76bae707283e54e98039dee1e0f6
Pablo-Ali <pablosharifali@gmail.com>
2025-05-26 18:00:52 -0300

    update: el modulo controlador ya posee su propia clase

--- nivel_2/proyecto/controlador.py
+++ nivel_2/proyecto/controlador.py
@@ -1,8 +1,15 @@
 from tkinter import Tk
 from vista import Ventana
 
+class Controlador:
+    def __init__(self):
+        self.root = Tk()
+        self.panel = Ventana()
+
+    def iniciar(self):
+        self.panel.vista_principal(self.root)
+        self.root.mainloop()
+
 if __name__ == "__main__":
-    root_tk = Tk()
-    panel = Ventana()
-    panel.vista_principal(root_tk)
-    root_tk.mainloop()
+    app = Controlador()
+    app.iniciar()
